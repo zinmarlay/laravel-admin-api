ad149e7ff90f56eb42b510062d5992c2b16455f5
ZinMarLwin <zimmar07@gmail.com>
2023-08-03 16:33:57 +0900

    users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,37 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Hash;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class UserController extends Controller
 {
  
     public function index()
     {
-        //
+        return User::all();
     }
 
   
     public function store(Request $request)
     {
-        //
+        $user = User::create(
+            $request->only('first-name','last-name','email')
+            + ['password' => Hash::make(1234)]
+        );
+        return response($user,Response::HTTP_CREATED);
     }
 
     
     public function show($id)
     {
-        //
+        return User::find($id);
     }
 
    
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+        $user->update($request->only('first-name','last-name','email'));
+        return response($user,Response::HTTP_ACCEPTED);
+
     }
 
   
     public function destroy($id)
     {
-        //
+        User::destroy($id);
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
